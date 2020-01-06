e3ba3776d6660c6706289b15278281f19cbdde5a
小聪忙 <1215524451@qq.com>
2020-01-06 10:11:15 +0800

    hooks基础demo

---
+++ react-hooks-demo01/src/Example2.js
@@ -0,0 +1,45 @@
+import React, {Component, useState} from 'react';
+
+// 不用hooks的写法如下:
+// class Example2 extends Component {
+//     constructor(props) {
+//         super(props);
+//         this.state = {count: 0}
+//         this.addCount = this.addCount.bind(this);
+//     }
+//     addCount() {
+//         this.setState({
+//             count: this.state.count + 1
+//         })
+//     }
+//     render() {
+//         return (
+//             <div>
+//                 <p>you click {this.state.count} times!</p>
+//                 <button onClick={this.addCount}>click me!</button>
+//             </div>
+//         );
+//     }
+//     componentDidMount() {
+//         console.log('==componentDidMount==')
+//     }
+//     componentDidUpdate(prevProps, prevState, snapshot) {
+//         console.log('==componentDidUpdate==','prevProps:',prevProps,'prevState:',prevState,'snapshot:',snapshot)
+//     }
+// }
+
+
+
+// 使用hooks的写法如下:
+
+function Example2(props) {
+    const [count, setCount] = useState(0);
+    return (
+        <div>
+            <p>you click {count} times!</p>
+            <button onClick={() => {setCount(count+1)}}></button>
+        </div>
+    );
+}
+
+export default Example2;
