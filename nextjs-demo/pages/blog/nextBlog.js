62136cf9a8fa4cb4c04d6e323774c4a82041cb0d
小聪忙 <1215524451@qq.com>
2020-01-07 11:08:25 +0800

    next.js 项目搭建

---
+++ nextjs-demo/pages/blog/nextBlog.js
@@ -0,0 +1,1 @@
+export default () => <div>nextBlog页面</div>
---
+++ nextjs-demo/pages/mango.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Mango(props) {
+    return (
+        <div>MangoGoing</div>
+    );
+}
+
+export default Mango;
